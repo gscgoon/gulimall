3db2ce341e43ee6209ec6f18049654a1ca679f4f
gscgoon <2713757376@qq.com>
2020-05-18 16:35:13 +0800

    商品三级分类

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -2,12 +2,13 @@ package com.atguigu.gulimall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallMemberApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GulimallMemberApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallMemberApplication.class, args);
+	}
 }
